2803bfd97d62aedf4219cf7135c0a19ee8bd0021
jintiantian <jintiantianbrandy@foxmail.com>
2019-06-06 17:58:38 +0800

    Update render-list.js

--- src/core/instance/render-helpers/render-list.js
+++ src/core/instance/render-helpers/render-list.js
@@ -13,6 +13,8 @@ export function renderList (
     index?: number
   ) => VNode
 ): ?Array<VNode> {
+  // val 可以是数组，字符串数字或者是对象
+  // 调用传入的render方法，返回的虚拟节点，并在返回数组ret中。（再那里传的?）
   let ret: ?Array<VNode>, i, l, keys, key
   if (Array.isArray(val) || typeof val === 'string') {
     ret = new Array(val.length)
@@ -25,6 +27,8 @@ export function renderList (
       ret[i] = render(i + 1, i)
     }
   } else if (isObject(val)) {
+    // 是否有唯一的标识，如果有迭代器，则使用迭代器遍历value
+    // 如果没有迭代器，那么则取出key来遍历
     if (hasSymbol && val[Symbol.iterator]) {
       ret = []
       const iterator: Iterator<any> = val[Symbol.iterator]()
